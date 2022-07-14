0ea62090b7d9de746ea9737aeeb000f53c6e8d19
Akan Daniel <51209193+AkanimohOD19A@users.noreply.github.com>
2022-07-14 21:46:06 +0100

    Updated Settings
    Added pythonanywhere to ALLOWED_HOST

--- QUIZ/settings.py
+++ QUIZ/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3!=&@-n+jaigmo4sjte1e%fh^0p8==g!+g%j5&t(rn2p&(jujg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['afrologicinsect.pythonanywhere.com',]
 
 
 # Application definition
