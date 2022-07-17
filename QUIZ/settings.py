19cde1ac4377a619f1f61a4a5157f2355a30ca4a
AfroLogicInsect <danielamahtoday@gmail.com>
2022-07-17 05:01:36 +0100

    initiated static t_setup

--- QUIZ/settings.py
+++ QUIZ/settings.py
@@ -119,8 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
+# STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
